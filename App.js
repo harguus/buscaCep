a15a87374c271280673a5271a0acee759a590d85
Thiago Cristiano Tavares <thiagocristiano.tavares@gmail.com>
2018-07-10 23:03:22 -0300

    troca da cor da barra superior de naveçaão para cor do rodapé e iconback e título para branco.

--- App.js
+++ App.js
@@ -9,12 +9,12 @@ import Sobre from './views/Sobre';
 export default class App extends Component<Props> {
   render() {
     return (
-        <Router>
+      <Router navigationBarStyle={{ backgroundColor: '#1E2D42' }} titleStyle={{ color: "#fff" }}>
           <Modal>
               <Scene hideNavBar={true} key='home' component={Home}/>
-              <Scene key="porcep" component={PorCep} title="Busca por CEP"/>
-              <Scene key="porrua" component={PorRua} title="Busca Por Rua"/>
-              <Scene key="sobre" component={Sobre} title="Sobre a aplicação"/>
+              <Scene headerTintColor='#fff' key="porcep" component={PorCep} title="Busca por CEP"/>
+              <Scene headerTintColor='#fff' key="porrua" component={PorRua} title="Busca Por Rua"/>
+              <Scene headerTintColor='#fff' key="sobre" component={Sobre} title="Sobre a aplicação"/>
           </Modal>
         </Router>
     );
